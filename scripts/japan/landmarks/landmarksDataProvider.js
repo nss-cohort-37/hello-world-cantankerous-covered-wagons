b3d4580e1438bf71765962542596013c0fc76067
philipDmartin <philipnathan788@gmail.com>
2019-12-03 11:25:17 -0600

    rendering html with javascript

---
+++ scripts/japan/landmarks/landmarksDataProvider.js
@@ -0,0 +1,18 @@
+const landmarksCollection = [
+    {
+        name: "Himeji Castle, Hyogo",
+    },
+    {
+        name: "Atomic Bomb Dome, Hiroshima"
+    },
+    {
+        name: "Senso-ji Temple, Tokyo",
+    }
+]
+
+export const useLandmarks = () => {
+    return landmarksCollection
+}
+
+
+
